1a44ed8e1b5eb1029c8b3ec0a05c432f6a3aa572
James Leung <jlfly12@gmail.com>
2020-12-22 17:39:08 +0000

    Theme change

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -9,24 +9,70 @@ import * as microsoftTeams from "@microsoft/teams-js";
  * The 'PersonalTab' component renders the main tab content
  * of your app.
  */
-export default  () => {
+export default () => {
   const [context, setContext] = useState<microsoftTeams.Context>();
+  const [newTheme, setNewTheme] = useState<{
+    backgroundColor?: string;
+    color?: string;
+  }>({});
+
+  const lightTheme = {
+    backgroundColor: "#EEF1F5",
+    color: "#16233A",
+  };
+
+  const darkTheme = {
+    backgroundColor: "#2B2B30",
+    color: "#FFFFFF",
+  };
+
   let userName = "";
-  if (context && "upn" in context) {
-    userName = context["upn"] ?? "";
+  if (context) {
+    userName = context.userPrincipalName ?? "";
   }
+
+  const updateTheme = (theme: string | undefined) => {
+    if (theme === "default") {
+      setNewTheme(lightTheme);
+    } else {
+      setNewTheme(darkTheme);
+    }
+  };
 
   useEffect(() => {
     microsoftTeams.getContext((context: microsoftTeams.Context) => {
-      setContext(context);
+      if (context) {
+        // Initial update to context and theme
+        updateTheme(context.theme);
+        setContext(context);
+        microsoftTeams.registerOnThemeChangeHandler((theme) => {
+          // Update theme when it is changed by user
+          if (theme !== context.theme) {
+            setContext({ ...context });
+          }
+          updateTheme(context.theme);
+        });
+      }
     });
-  }, []);
+  }, [JSON.stringify(context), JSON.stringify(newTheme)]);
 
   return (
-    <div>
-      <h3>Hello World!</h3>
-      <h1>Congratulations {userName}!</h1>{" "}
-      <h3>This is the tab you made :-)</h3>
+    <div style={newTheme}>
+      <p>Username: {userName}</p>
+      <h1>Important Contacts</h1>
+      <ul>
+        <li>
+          Help Desk:{" "}
+          <a href="mailto:support@company.com">support@company.com</a>
+        </li>
+        <li>
+          Human Resources: <a href="mailto:hr@company.com">hr@company.com</a>
+        </li>
+        <li>
+          Facilities:{" "}
+          <a href="mailto:facilities@company.com">facilities@company.com</a>
+        </li>
+      </ul>
     </div>
   );
 };
